e6e96e5aecedb5989e5ed85fb5e551a87c37e5bc
Steve Bennett <stevage@gmail.com>
2014-02-13 21:09:18 +1100

    ...code code code...

--- osm/config-postgis.sh
+++ osm/config-postgis.sh
@@ -13,18 +13,18 @@ FOF
 
 
 # create GIS template
-db=template_gis
-sudo -E -su postgres bash <<EOF
-createdb --encoding=UTF8 --owner=$tm_dbusername $db
-psql -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='$db'"
+template=template_gis
+sudo -E -su postgres <<EOF
+createdb --encoding=UTF8 --owner=$tm_dbusername $db --template=$template
+psql -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='$template'"
 
 psql -d $db -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql > /dev/null
 psql -d $db -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql > /dev/null
 psql -d $db -f /usr/share/postgresql/9.1/contrib/postgis_comments.sql > /dev/null
 psql -d $db -c "GRANT SELECT ON spatial_ref_sys TO PUBLIC;"
 psql -d $db -c "GRANT ALL ON geometry_columns TO $tm_dbusername;"
-psql -d $db -c "create extension hstore;"
+psql -d $db -c "CREATE EXTENSION hstore;"
 EOF
 
-sudo -E -u postgres createdb --template=$db gis
+sudo -E -u postgres createdb --template=$template gis
 sudo -E -u postgres psql -d gis -c "GRANT ALL ON DATABASE gis TO $tm_dbusername;"
