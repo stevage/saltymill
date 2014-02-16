27bd4d65b2d35199ffff36d502bdaee46039e196
Steve Bennett <stevage@gmail.com>
2014-02-17 09:07:59 +1100

    ...code code code...

--- log.sh
+++ log.sh
@@ -1,3 +1,3 @@
 # Adds a line of log output into the log HTML file
-echo "$1" > /var/log/salt/buildlog.html
-sed -i 's/<!-- logend -->/$1\n<!-- logend -->/' /var/log/salt/index.html
+echo "$1" >> /var/log/salt/buildlog.html
+sed -i "s/<!-- logend -->/$1\n<!-- logend -->/" /var/log/salt/index.html
