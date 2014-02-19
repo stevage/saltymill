a687266943919a9d3c112ce3e958fb0d8f2b8126
Steve Bennett <stevage@gmail.com>
2014-02-19 15:26:36 +1100

    ...code code code...

---
+++ turbo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo Turbo installer for SaltyMill
+curl -Ls http://bootstrap.saltstack.org | sudo sh
+echo fqdn: `curl -s http://ifconfig.me` | sudo tee /etc/salt/grains > /dev/null
+sudo service salt-minion restart
+
+sudo apt-get install -qqy git && 
+sudo git clone --quiet --depth 1 https://github.com/stevage/saltymill /srv/salt
+
+cd /srv/salt
+sudo cp -R pillar /srv/
+
+echo "You should edit /srv/pillar/tm.sls now. Comments are in the file."
+echo Type "skip" to skip or anything else to edit.
+read text
+if [ "$text" != "skip" ]; then
+sudo pico /srv/pillar/tm.sls
+fi
+sudo salt-call --local state.highstate -l info
